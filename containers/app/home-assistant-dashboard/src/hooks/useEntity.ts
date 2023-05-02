08056de389de0d988f358fe79adb16dc7a283776
wvanheemstra <wvanheemstra@users.noreply.github.com>
2023-05-02 13:15:38 +0200

    Create useEntity.ts

---
+++ containers/app/home-assistant-dashboard/src/hooks/useEntity.ts
@@ -0,0 +1,10 @@
+import { isEqual } from 'lodash';
+import { useHass } from '@hooks';
+
+export function useEntity(_entity: string) {
+    const entity = useHass(state => state.entities[_entity], (oldEntity, newEntity) => isEqual(oldEntity, newEntity));
+    if (!entity) {
+      throw new Error(`Entity "${_entity}" does not exist`);
+    }
+    return entity;
+}
